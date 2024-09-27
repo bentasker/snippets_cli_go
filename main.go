90d4819fe595d602ee01662782f409b97fd5cb31
B Tasker <github@bentasker.co.uk>
2024-09-27 09:15:31 +0100

    feat: add custom user-agent

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "github.com/mmcdole/gofeed"
 
 func main() {
     fp := gofeed.NewParser()
+    fp.UserAgent = "snippets_cli_go"
     feed, _ := fp.ParseURL("https://www.bentasker.co.uk/rss.xml")
     fmt.Println(feed.Title)
 }
