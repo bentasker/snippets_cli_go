d7aec92b065c45b8ad26c9a0fa0ca3e9e44dccdc
B Tasker <github@bentasker.co.uk>
2024-09-27 10:27:39 +0100

    feat: iterate through feed items extracting the bits that we'll want to look at (utilities/snippets_cli_go#2)
    This doesn't yet do anything particularly useful, but shows how we'll iterate through once searching starts
    being implemented

--- main.go
+++ main.go
@@ -3,9 +3,26 @@ package main
 import "fmt"
 import "github.com/mmcdole/gofeed"
 
-func main() {
+
+func fetchFeed() (feed *gofeed.Feed, err error){
+    // TODO: caching
     fp := gofeed.NewParser()
     fp.UserAgent = "snippets_cli_go"
-    feed, _ := fp.ParseURL("https://www.bentasker.co.uk/rss.xml")
+    return fp.ParseURL("https://www.bentasker.co.uk/rss.xml")
+}
+
+
+func main() {
+    
+    feed, _ := fetchFeed()
     fmt.Println(feed.Title)
+    
+    for _, item := range feed.Items{
+        fmt.Println(item.Title)
+        fmt.Println(item.Link)
+        //fmt.Println(item.Description)
+        for _, cat := range item.Categories{
+            fmt.Println(cat)
+        }
+    }
 }
