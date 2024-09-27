694e17d54fd6fa031bbffbddb64388a4df647b62
B Tasker <github@bentasker.co.uk>
2024-09-27 12:40:23 +0100

    feat: fetch page and start extracting info (utilities/snippets_cli_go#6)

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/jedib0t/go-pretty/v6 v6.5.9 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/k3a/html2text v1.2.1 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mmcdole/gofeed v1.3.0 // indirect
 	github.com/mmcdole/goxpp v1.1.1-0.20240225020742-a0c311522b23 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+    "bytes"
     "fmt"
+    "log"
+    "net/http"
     "os"
     "strconv"
     "strings"
     
     "github.com/jedib0t/go-pretty/v6/table"
+    "github.com/k3a/html2text"
     "github.com/mmcdole/gofeed"
+    "golang.org/x/net/html"
 )
 
 type searchResult struct{
@@ -28,6 +33,71 @@ func fetchFeed() (feed *gofeed.Feed, err error){
     return fp.ParseURL("https://www.bentasker.co.uk/rss.xml")
 }
 
+
+/** Fetch a snippet and process it into something which can be
+ * output to console
+ * 
+ */
+func printSnippet(id int, title string, link string){
+    // Fetch the page
+    fmt.Println(link)
+    resp, err := http.Get(link)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer resp.Body.Close()
+    
+    if resp.StatusCode != 200 {
+        log.Fatalf("Failed with Status code: %d %s", resp.StatusCode)
+    }
+    
+    // Parse the page
+    doc, err := html.Parse(resp.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+    
+    
+    // Define the map that we'll write content into
+    entries := make(map[string]*html.Node)
+    
+    // Iterate through to select the items we need
+    var f func(*html.Node, map[string]string)
+    f = func(n *html.Node, elems map[string]string){
+                
+        
+        // TODO Only check attributes of divs
+        if n.Type == html.ElementNode && n.Data == "h4" {
+            attrs := n.Attr
+            for _, attr := range attrs {
+                    if attr.Key == "id" {
+                        // See if it's in the list of desired IDs
+                        _, desired := elems[attr.Val]
+                        if desired {
+                            entries[attr.Val] = n
+                        }
+                    }
+                }
+        }
+    
+        // Recurse through
+        for c := n.FirstChild; c != nil; c = c.NextSibling {
+            f(c, elems)
+        }
+    }    
+
+    // Map HTML IDs to output titles
+    desiredElements := make(map[string]string)
+    desiredElements["conclusion"] = "Conclusion"
+        
+    f(doc, desiredElements)
+    _, ok := entries["conclusion"]; if ok {
+        var h bytes.Buffer
+        html.Render(&h, entries["conclusion"])
+        fmt.Println(html2text.HTML2Text(h.String()))
+    }
+
+}
 
 func printTable(res []searchResult, s search) {
     
@@ -99,6 +169,7 @@ func searchFeed(feed *gofeed.Feed, search search) []searchResult{
             if id == searchID {
                 // TODO - print the snippet
                 fmt.Println("%i matches", id)
+                printSnippet(id, item.Title, item.Link)
             }
         }
     
